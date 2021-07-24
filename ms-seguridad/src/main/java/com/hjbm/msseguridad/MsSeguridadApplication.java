fafaa71743a5f319eb3c96c8a6c26ab8f7f3e99e
Héctor Bracho Mendoza <hbracho55@hotmail.com>
2021-07-24 18:52:26 -0400

    inclusion servicio requerimiento dependencias cloudshabilitar cliente de eureka

--- ms-seguridad/src/main/java/com/hjbm/msseguridad/MsSeguridadApplication.java
+++ ms-seguridad/src/main/java/com/hjbm/msseguridad/MsSeguridadApplication.java
@@ -2,8 +2,10 @@ package com.hjbm.msseguridad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsSeguridadApplication {
 
 	public static void main(String[] args) {
