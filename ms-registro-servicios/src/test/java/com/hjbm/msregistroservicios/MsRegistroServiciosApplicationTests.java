cea57ba54a9dd92121962d07f261fddb90d7766e
Héctor Bracho Mendoza <hbracho55@hotmail.com>
2021-07-24 18:21:54 -0400

    agregando servicio eureka

---
+++ ms-registro-servicios/src/test/java/com/hjbm/msregistroservicios/MsRegistroServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjbm.msregistroservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistroServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
